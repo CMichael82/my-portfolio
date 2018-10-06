286f869db4ba1a2cd4e3d0a80a235d47ac53aa99
CAM <34009133+CMichael82@users.noreply.github.com>
2018-10-05 21:34:44 -0600

    updated styling and added new HW to portfolio

---
+++ js/logic.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	$('section').eq(0).show();
+	$('.navbar-nav').on('click', 'li', function() {
+			$('section').eq($(this).index()).show().siblings('section:visible').hide();
+	});
+});
